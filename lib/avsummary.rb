ec897247aaa1ad4844669285d03de83623eb3b00
Hiroyuki Mishima <missy@be.to>
2012-11-09 11:54:39 +0900

    typo: run-annoteta-varia<r>tion

--- lib/avsummary.rb
+++ lib/avsummary.rb
@@ -12,7 +12,7 @@ VERSION = "20121101"
 module AvSummary
   AVCONFIG = "avconfig"
   AVCONVERT = "run-convert2annovar.sh"
-  AVSCRIPT = "run-annotate-variartion.sh"
+  AVSCRIPT = "run-annotate-variation.sh"
   BATCHSIZE = "50m"
   CHR_ORDER =
     %w(M 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y) \
